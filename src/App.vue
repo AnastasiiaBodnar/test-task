8580e915ed63d59e98c32a2f044e0b74f7609947
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-04-02 23:52:58 +0300

    feat: add AppHeader template structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
-<script setup></script>
+<script setup>
+import AppHeader from './components/layout/AppHeader.vue';
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <AppHeader />
+  <RouterView />
 </template>
 
 <style scoped></style>
--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>Header</div>
+  <header class="header">
+    <div class="header__container">
+        <div class="header__logo">LOGO</div>
+
+        <nav class="header__nav">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Pricing</a>
+            <a href="#">Facts</a>
+            <a href="#">Blog</a>
+            <a href="#">Support</a>
+        </nav>
+
+        <div class="header-actions">
+            <button class="header__btn">Download</button>
+            <div class="header__lang">Eng ▾</div>
+        </div>
+    </div>
+  </header>
 </template>
