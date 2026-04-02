48cc9999bf08d60a430dd1637ff41e2da9b55490
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-04-02 23:34:46 +0300

    feat: configure router with home and form routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '@/views/HomePage.vue'
+import FormPage from '@/views/FormPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', component: HomePage},
+    { path: '/form', component: FormPage},
+  ],
 })
 
 export default router
