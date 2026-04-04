59209d6fea03ef1aa45f84305b5daf329349a616
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-04-04 22:29:41 +0300

    feat: add AppFooter template structure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
+import AppFooter from './components/layout/AppFooter.vue';
 import AppHeader from './components/layout/AppHeader.vue';
 </script>
 
 <template>
   <AppHeader />
   <RouterView />
+  <AppFooter />
 </template>
 
 <style scoped></style>
--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,3 +1,29 @@
 <template>
-  <div>Footer</div>
+  <footer class="footer">
+    <div class="footer__container"> 
+      <div class="footer__logo">
+      Logo
+      </div>
+
+      <nav class="footer__nav">
+        <div class="footer__nav-col">
+          <a href="#">About us</a>
+          <a href="#">Blog</a>
+          <a href="#">Support</a>
+        </div>
+
+        <div class="footer__nav-col">
+          <a href="#">Terms & Conditions</a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Cookies</a>
+        </div>
+      </nav>
+
+      <div class="footer__social">
+        <a href="#"><img src="/src/assets//icons/Instagram.png" alt="Instagram"></a>
+        <a href="#"><img src="/src/assets/icons/YouTube.png" alt="YouTube"></a>
+        <a href="#"><img src="/src/assets/icons/Pinterest.png" alt="Pinterest"></a>
+      </div>
+    </div>
+  </footer>
 </template>
