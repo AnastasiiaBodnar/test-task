68f4164f7607ca8a54284aede3040842b0bd3a02
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-04-04 23:13:02 +0300

    feat: add AppFooter styles

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,23 @@ import AppHeader from './components/layout/AppHeader.vue';
 </script>
 
 <template>
-  <AppHeader />
-  <RouterView />
-  <AppFooter />
+  <div class="app-wrapper">
+    <AppHeader />
+    <main class="main-content">
+      <RouterView />
+    </main>
+    <AppFooter />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app-wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main-content {
+  flex: 1;
+}
+</style>
--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div class="footer__container"> 
       <div class="footer__logo">
-      Logo
+      LOGO
       </div>
 
       <nav class="footer__nav">
@@ -20,10 +20,62 @@
       </nav>
 
       <div class="footer__social">
-        <a href="#"><img src="/src/assets//icons/Instagram.png" alt="Instagram"></a>
+        <a href="#"><img src="/src/assets/icons/Facebook.png" alt="Facebook"></a>
+        <a href="#"><img src="/src/assets/icons/Instagram.png" alt="Instagram"></a>
         <a href="#"><img src="/src/assets/icons/YouTube.png" alt="YouTube"></a>
         <a href="#"><img src="/src/assets/icons/Pinterest.png" alt="Pinterest"></a>
       </div>
     </div>
   </footer>
 </template>
+
+<style scoped>
+
+.footer {
+  background: #423C4E;
+}
+
+.footer__container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 60px 24px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+}
+
+.footer__logo {
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 100%;
+  color: var(--color-white);
+}
+
+.footer__nav {
+  display: flex;
+  gap: 180px;
+}
+
+.footer__nav-col {
+  display: flex;
+  flex-direction: column;
+}
+
+.footer__nav-col a {
+  text-decoration: none;
+  color: var(--color-white);
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 50px;
+}
+
+.footer__social {
+  display: flex;
+  gap: 16px;
+}
+
+.footer__social img {
+  width: 32px;
+  height: 32px;
+}
+</style>
