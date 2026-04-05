0d3a8e97e3b4fbd42cf6a8ad3b1ff0fb7209a0c0
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-04-05 23:46:50 +0300

    feat: add HowItWork section template structure

---
+++ src/components/home/HowItWorks.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="how">
+        <div class="how_container">
+           <h2 class="how__title">How does it work?</h2> 
+           <p class="how__subtitle">Easy, fun & essential training for moms</p>
+           <div class="how__divider"></div>
+
+           <div class="how__items">
+            <div class="how__item">
+                <img src="/src/assets/icons/exercises.png" alt="exercises">
+                <div class="how__item-content">
+                    <div class="how__item-title">Browse exercises</div>
+                    <div class="how__item-text">Browse over 100 exercises with a video or picture and a description.</div>
+                </div>
+            </div>
+
+            <div class="how__item">
+                <img src="/src/assets/icons/timeline.png" alt="timeline">
+                <div class="how__item-content">
+                    <div class="how__item-title">Create timeline</div>
+                    <div class="how__item-text">Take pictures along the way during and after the pregnancy to see the transformation.</div>
+                </div>
+            </div>
+           </div>
+        </div>
+    </section>
+</template>
+
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeroSection from '@/components/home/HeroSection.vue';
+import HowItWorks from '@/components/home/HowItWorks.vue';
 </script>
 
 <template>
   <div>
     <HeroSection />
+    <HowItWorks />
   </div>
 </template>
