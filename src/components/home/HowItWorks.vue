0a15c66ab9af2e11104d4286cdeca2469beb955f
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-04-06 23:55:39 +0300

    feat: add styles for HowItemWorks section component

--- src/components/home/HowItWorks.vue
+++ src/components/home/HowItWorks.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="how">
-        <div class="how_container">
+        <div class="how__container">
            <h2 class="how__title">How does it work?</h2> 
            <p class="how__subtitle">Easy, fun & essential training for moms</p>
            <div class="how__divider"></div>
@@ -26,3 +26,67 @@
     </section>
 </template>
 
+
+<style scoped>
+
+.how__container {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 30px 0;
+    text-align: center;
+}
+
+.how__title {
+    font-size: 45px;
+    font-weight: 700;
+    line-height: 100%;
+    color: var(--color-dark-text);
+}
+
+.how__subtitle {
+    font-size: 28px;
+    font-weight: 500;
+    line-height: 100%;
+    color: var(--color-light-text);
+}
+
+.how__divider {
+    width: 45px;
+    height: 3px;
+    background-color: var(--color-divider, #FF2D55);
+    margin: 20px auto 30px auto;
+}
+
+.how__items {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+
+.how__item {
+    display: flex;
+    align-items: flex-start;
+    gap: 20px;
+    text-align: left;
+}
+
+.how__item img {
+    display: flex;
+    line-height: 1
+}
+
+.how__item-title {
+    font-size: 28px;
+    font-weight: 500;
+    color: #23153E;
+    line-height: 1.8;
+}
+
+.how__item-text {
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 28px;
+    color: #443A5A;
+    max-width: 480px;
+}
+</style>
