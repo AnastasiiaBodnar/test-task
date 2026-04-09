21a58ab57221f239d57c01064ef1ee3b922d4762
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-04-09 22:57:15 +0300

    feat: add styles for PregnancyStages section component

--- src/components/home/PregnancyStages.vue
+++ src/components/home/PregnancyStages.vue
@@ -55,3 +55,137 @@
     </div>
   </section>
 </template>
+
+
+<style scoped>
+.stages {
+  padding: 60px 0;
+}
+
+.stages__container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.stages__header {
+  text-align: center;
+  margin-bottom: 60px;
+}
+
+.stages__title {
+  font-size: 50px;
+  font-weight: 700;
+  line-height: 100%;
+  color: var(--color-dark-text);
+  margin: 20px 0 20px 0;
+}
+
+.stages__subtitle {
+  font-size: 32px;
+  font-weight: 500;
+  color: var(--color-light-text);
+  margin: 0 auto;
+  line-height: 100%;
+}
+
+.stages__divider {
+  width: 45px;
+  height: 3px;
+  background-color: var(--color-divider);
+  margin: 20px auto 30px auto;
+}
+
+.stages__row {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 60px;
+  align-items: center;
+  position: relative;
+  z-index: 1;
+}
+
+.stages__row--right {
+  margin-bottom: 40px;
+}
+
+.stages__row--left {
+  margin-top: 40px;
+}
+
+.stages__row--left .stages__phone {
+  grid-column: 1;
+  grid-row: 1;
+}
+
+.stages__row--left .stages__content {
+  grid-column: 2;
+  grid-row: 1;
+}
+
+.stages__content {
+  padding-top: 80px;
+}
+
+.stages__stage-title {
+  font-size: 38px;
+  font-weight: 700;
+  color: var(--color-dark-text);
+  margin: 0 0 16px 0;
+}
+
+.stages__divider-stage {
+  width: 45px;
+  height: 3px;
+  background-color: var(--color-divider);
+  margin: 16px auto 18px 0;
+}
+
+.stages__stage-subtitle {
+  font-size: 26px;
+  font-weight: 600;
+  color: var(--color-light-text);
+  margin: 20px 0 10px 0;
+  line-height: 100%;
+}
+
+.stages__stage-text {
+  font-size: 18px;
+  font-weight: 400;
+  color: var(--color-light-text);
+  line-height: 28px;
+  margin: 0 0 12px 0;
+}
+
+.stages__phone {
+  display: flex;
+  justify-content: center;
+}
+
+.stages__phone img {
+  width: 550px;
+  height: auto;
+  object-fit: contain;
+}
+
+.stages__watermark-wrap {
+  position: relative;
+  height: 0;
+  overflow: visible;
+  z-index: 0;
+}
+
+.stages__watermark {
+  position: absolute;
+  left: 60%;
+  transform: translateX(-50%);
+  top: -90px;
+  font-size: 220px;
+  font-weight: bolder;
+  color: #F8F7FC;
+  line-height: 1;
+  white-space: nowrap;
+  user-select: none;
+  pointer-events: none;
+  letter-spacing: -6px;
+}
+</style>
