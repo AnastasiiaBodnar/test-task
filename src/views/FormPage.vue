6a33fe9b3caaf88148261792c0c4aa3cc4c055a6
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-04-14 21:49:57 +0300

    feat: add FormPage template structure

--- src/views/FormPage.vue
+++ src/views/FormPage.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>FormPage</div>
+  <section class="form">
+    <div class="form__container">
+      <div class="form__card">
+        <h2 class="form__title">Send your data</h2>
+
+        <input class="form__input" type="text" placeholder="First Name">
+        <input class="form__input" type="text" placeholder="Second Name">
+        <input class="form__input" type="email" placeholder="Email">
+        <input class="form__input" type="text" placeholder="Adress">
+
+        <button class="form__btn">Submit</button>
+      </div>
+    </div>
+  </section>
 </template>
