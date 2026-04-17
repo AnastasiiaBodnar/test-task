cf10583ce92ec17c2cf3e118070266720af7421e
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-04-17 17:18:06 +0300

    refactor: BlogPosts data extraction

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,6 +4,53 @@ import CtaBanner from '@/components/home/CtaBanner.vue';
 import HeroSection from '@/components/home/HeroSection.vue';
 import HowItWorks from '@/components/home/HowItWorks.vue';
 import PregnancyStages from '@/components/home/PregnancyStages.vue';
+
+import runningImg from '@/assets/image/running.png'
+import avatarImg from '@/assets/image/avatar.png'
+import walksImg from '@/assets/image/walks.png'
+import workoutImg from '@/assets/image/workout.png'
+import fitnessImg from '@/assets/image/fitness.png'
+import therapistImg from '@/assets/image/therapist.png'
+
+const posts = [
+  {
+    id: 1,
+    title: 'Return to running after pregnancy – Is your body strong enough for running?',
+    author: 'BY TILDA LOFTIN',
+    avatar: avatarImg,
+    date: 'JUNE 25, 2021',
+    image: runningImg,
+    large: true,
+  },
+  {
+    id: 2,
+    title: 'Stroller walks',
+    date: 'APRIL 21, 2021',
+    image: walksImg,
+    large: false,
+  },
+  {
+    id: 3,
+    title: 'Playground workout',
+    date: 'MARCH 17, 2020',
+    image: workoutImg,
+    large: false,
+  },
+  {
+    id: 4,
+    title: 'Your Essential Fitness Gear for Each Trimester',
+    date: 'FEBRUARY 21, 2021',
+    image: fitnessImg,
+    large: false,
+  },
+  {
+    id: 5,
+    title: 'What YOU could gain by working with a Woman’s Health Physical Therapist ',
+    date: 'JANUARY 23, 2020',
+    image: therapistImg,
+    large: false,
+  },
+]
 </script>
 
 <template>
@@ -11,7 +58,7 @@ import PregnancyStages from '@/components/home/PregnancyStages.vue';
     <HeroSection />
     <HowItWorks />
     <PregnancyStages />
-    <BlogPosts />
+    <BlogPosts :posts="posts" />
     <CtaBanner />
   </div>
 </template>
